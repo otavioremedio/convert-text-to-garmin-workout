1d2772fb49810757baee3c24633557ebe767ea2b
otavio <otavioremedio@gmail.com>
2025-06-18 12:48:57 -0300

    duplica lançamento pra corrigir problema com script do site

--- src/main/kotlin/service/GarminService.kt
+++ src/main/kotlin/service/GarminService.kt
@@ -8,7 +8,9 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.interactions.Actions
 import org.openqa.selenium.support.ui.Select
+
 
 class GarminService {
 
@@ -26,11 +28,11 @@ class GarminService {
         }
 
         driver.findElement(By.className("icon-pencil")).click()
-        Thread.sleep(500)
+        Thread.sleep(200)
         driver.findElement(By.className("WorkoutName_workoutName__E7sq8")).sendKeys(name)
-        Thread.sleep(500)
+        Thread.sleep(200)
         driver.findElement(By.className("icon-checkmark")).click()
-        Thread.sleep(500)
+        Thread.sleep(200)
         driver.findElement(By.className("Button_primary__7zt4j")).click()
         Thread.sleep(2000)
         driver.findElement(By.className("Button_primary__7zt4j")).click()
@@ -40,7 +42,7 @@ class GarminService {
         val btnDelete = driver.findElements(By.className("icon-trash"))
         btnDelete.forEach {
             it.click()
-            Thread.sleep(500)
+            Thread.sleep(200)
             driver.findElement(By.className("Button_danger__hwLq8")).click()
         }
     }
@@ -58,24 +60,24 @@ class GarminService {
                     inputTimes.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE))
                     inputTimes.sendKeys(step.repeat.toString())
                     divRepeat.findElements(By.className("WorkoutStep_stepFieldsWrapper__Dmd26")).first().click()
-                    Thread.sleep(500)
+                    Thread.sleep(200)
                     first = false
                 } else {
                     divRepeat = driver.findElements(By.className("WorkoutRepeatStep_repeatStepWrapper__s9T1j")).last()
                     divRepeat.findElements(By.className("WorkoutStep_stepFieldsWrapper__Dmd26")).last().click()
                     Select(divRepeat.findElement(By.id("select-details"))).selectByValue("interval")
                     Select(divRepeat.findElement(By.id("select-duration-rest-options"))).selectByValue("time")
-                    Thread.sleep(500)
+                    Thread.sleep(200)
                 }
             } else {
                 driver.findElements(By.className("Button_small__waifo")).first().click()
-                Thread.sleep(500)
+                Thread.sleep(200)
                 driver.findElements(By.className("WorkoutStep_stepReadOnlyWrapper__u3DgD")).last().click()
             }
-            Thread.sleep(500)
+            Thread.sleep(200)
 
             Select(driver.findElement(By.id("select-primary-target"))).selectByValue("heart.rate.zone")
-            Thread.sleep(500)
+            Thread.sleep(200)
             if (it.time.contains("min")) {
                 if(it.time.contains(":")) {
                     val timeSplited = it.time.split(":")
@@ -90,17 +92,35 @@ class GarminService {
                 driver.findElement(By.className("TimeDurationInput_seconds__n9cXY")).sendKeys(it.time.filter{t -> t.isDigit() })
             }
 
-            Thread.sleep(500)
+            Thread.sleep(200)
             val hrInput: WebElement = driver.findElement(By.id("target-hr-input"))
-            hrInput.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE))
-            Thread.sleep(500)
             val hrTo: WebElement = driver.findElement(By.id("target-hr-to"))
-            hrTo.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE))
-            Thread.sleep(500)
-            hrTo.sendKeys((it.bpmMax).toString())
-            Thread.sleep(500)
-            hrInput.sendKeys(it.bpmMin.toString())
-            Thread.sleep(500)
+            Thread.sleep(200)
+            val actions = Actions(driver)
+            actions.click(hrTo)
+                .keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL)
+                .sendKeys(Keys.DELETE)
+                .sendKeys(it.bpmMax.toString())
+                .sendKeys(Keys.RETURN)
+                .perform()
+            actions.click(hrInput)
+                .keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL)
+                .sendKeys(Keys.DELETE)
+                .sendKeys(it.bpmMin.toString())
+                .sendKeys(Keys.RETURN)
+                .perform()
+            actions.click(hrTo)
+                .keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL)
+                .sendKeys(Keys.DELETE)
+                .sendKeys(it.bpmMax.toString())
+                .sendKeys(Keys.RETURN)
+                .perform()
+            actions.click(hrInput)
+                .keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL)
+                .sendKeys(Keys.DELETE)
+                .sendKeys(it.bpmMin.toString())
+                .sendKeys(Keys.RETURN)
+                .perform()
             driver.findElement(By.className("WorkoutStep_doneButton__xJZOm")).click()
         }
     }
